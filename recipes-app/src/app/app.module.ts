97b24d608d41d88fa6e88a67979eae2f25792596
halcyoff <halcyoff@gmail.com>
2020-10-09 20:55:53 -0500

    added material menu and ripple. added menu in separate component.

--- recipes-app/src/app/app.module.ts
+++ recipes-app/src/app/app.module.ts
@@ -1,29 +1,35 @@
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app-routing.module';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatRippleModule } from '@angular/material/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTableModule } from '@angular/material/table';
 
 import { AppComponent } from './app.component';
+import { HomeMenuComponent } from './components/home-menu/home-menu.component';
 import { HomescreenComponent } from './components/homescreen/homescreen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeMenuComponent,
     HomescreenComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule,
+    BrowserModule,
     MatExpansionModule,
-    MatTableModule
+    MatIconModule,
+    MatMenuModule,
+    MatRippleModule,
+    MatTableModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
