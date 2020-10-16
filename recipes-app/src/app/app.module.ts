5ef9bd397e6530eec45a409f66dabe99adde8ee9
halcyoff <halcyoff@gmail.com>
2020-10-16 09:13:20 -0500

    Imported new file for material modules

--- recipes-app/src/app/app.module.ts
+++ recipes-app/src/app/app.module.ts
@@ -1,14 +1,8 @@
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { materialModules } from './angular-material-modules';
 import { NgModule } from '@angular/core';
-
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatRippleModule } from '@angular/material/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTableModule } from '@angular/material/table';
 
 import { AppComponent } from './app.component';
 import { HomeMenuComponent } from './components/home-menu/home-menu.component';
@@ -18,18 +12,13 @@ import { HomescreenComponent } from './components/homescreen/homescreen.componen
   declarations: [
     AppComponent,
     HomeMenuComponent,
-    HomescreenComponent
+    HomescreenComponent,
+    materialModules
   ],
   imports: [
     AppRoutingModule,
     BrowserAnimationsModule,
-    BrowserModule,
-    MatExpansionModule,
-    MatIconModule,
-    MatMenuModule,
-    MatRippleModule,
-    MatTableModule,
-    MatToolbarModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
