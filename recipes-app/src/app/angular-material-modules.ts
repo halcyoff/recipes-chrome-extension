34bc27a6c8d0ad2132145cc5ed1d8d3ce5832bc2
halcyoff <halcyoff@gmail.com>
2020-10-16 09:09:44 -0500

    Moved material modules to new file.  Added MatListModule.

---
+++ recipes-app/src/app/angular-material-modules.ts
@@ -0,0 +1,17 @@
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatRippleModule } from '@angular/material/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+export const materialModules = [
+    MatExpansionModule,
+    MatIconModule,
+    MatListModule,
+    MatMenuModule,
+    MatRippleModule,
+    MatTableModule,
+    MatToolbarModule
+];
